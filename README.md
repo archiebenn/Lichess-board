5bbaf8d82209fc7d4f16e9edf3abde728e2ba2c2
archiebenn <archiesnbenn@gmail.com>
2026-03-01 17:59:48 +0000

    api script

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Lichess Board
 # analogue_lichess
+
+# pre-filled API token form:  
+https://lichess.org/account/oauth/token/create?scopes[]=challenge:write&scopes[]=puzzle:read&scopes[]=puzzle:write&scopes[]=board:play&description=Prefilled+token+example
---
+++ pi/lichess_api.py
@@ -0,0 +1,12 @@
+import berserk
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+# set up the client with lichess api token from the .env file
+session = berserk.TokenSession(os.getenv("lichess_token"))
+client = berserk.Client(session=session)
+
+# print your profile to confirm it works
+print(client.account.get())
